244069737f7df65639ae4a63a7a3fbc0b483797d
Srikanth <srikanth.kunkulagunta@gmail.com>
2013-10-15 17:25:40 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ChatApp::Application.config.secret_key_base = 'f6319a29eefe5561913aa02abc1e3d9d22cb932df392f3b5cbb45c1f0989d1faf92b0b209af4b7724fca29b8077e3f85dedd87a391ad49b3dfbb1ea34b16adac'
